a39bae8447b31065eacf14e3e1156fb67b807e1a
张柳平 <zlpyde@163.com>
2020-02-02 19:07:50 +0800

    :rocket: 第一次提交

--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,68 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Head from 'next/head'
-import Nav from '../components/nav'
+import {Row,Col,List,Icon} from 'antd'
+import axios from 'axios'
+import Header from '../components/Header'
+import Author from '../components/Author'
+import Advert from '../components/Advert'
+import Footer from '../components/Footer'
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+import '../public/style/pages/index.css'
 
-    <Nav />
 
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+const Home = (list) => {
 
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
-      </div>
+  const [mylist,setMylist] = useState(list.data)
+
+
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <Header />
+      <Row className="comm-main" type="flex" justify="center">
+        <Col className="comm-left" xs={24} sm={24} md={16} lg={18} xl={14}>
+          <List
+            header={<div>最新日志</div>}
+            itemLayout="vertical"
+            dataSource={mylist}
+            renderItem={item=>{
+              return (
+                <List.Item>
+                  <div className="list-title">{item.title}</div>
+                  <div className="list-icon">
+                    <span><Icon type="calendar" /> {item.addTime} </span>
+                    <span><Icon type="folder" /> {item.typeName} </span>
+                    <span><Icon type="fire" /> {item.view_count} </span>
+                  </div>
+                  <div className="list-context">{item.introduce}</div>
+                </List.Item>
+              )
+            }} />
+            
+            
+        </Col>
+        <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
+          <Author />
+          <Advert />
+        </Col>
+      </Row>
+      <Footer />
     </div>
+  )
+}
 
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
+Home.getInitialProps = async ()=> {
+  const promise = new Promise((resolve)=> {
+    axios('http://127.0.0.1:7001/default/getArticleList').then(
+      (res)=> {
+        console.log(res.data)
+        resolve(res.data)
       }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-)
+    )
+  })
+  return await promise
+}
 
 export default Home
